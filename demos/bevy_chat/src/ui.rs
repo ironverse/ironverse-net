a3461fe353218a8b9571793725b6425655e9e5ae
Hugo Lindsay <ashneverdawn@gmail.com>
2022-05-07 17:30:26 -0400

    enable copying of chat text

--- demos/bevy_chat/src/ui.rs
+++ demos/bevy_chat/src/ui.rs
@@ -23,7 +23,9 @@ pub fn chat_event_reader(mut local: ResMut<ChatResource>, mut chat_reader: Event
         if event.message.starts_with("/name ") && event.message.len() > 6 {
             let old_name = get_username(&local.name_map, event.sender.clone());
             let new_name = event.message[6..].to_string();
-            local.name_map.insert(event.sender.clone(), new_name.clone());
+            if !event.is_outgoing {
+                local.name_map.insert(event.sender.clone(), new_name.clone());
+            }
             local.chat_history.add_line(ChatEvent { 
                 is_outgoing: event.is_outgoing, 
                 sender: old_name, 
@@ -47,23 +49,28 @@ fn get_username(name_map: &HashMap<String, String>, sender: String) -> String {
         None => String::from(""),
     };
     let id = if sender.len() >= 4 {
-        format!("#{}", sender[sender.len()-4..].to_string())
+        format!("{}", sender[sender.len()-4..].to_string())
     } else {
         sender
     };
-    format!("{}{}", name, id)
+    if name.len() == 0 {
+        format!("{}", id)
+    } else {
+        format!("{}/{}", name, id)
+    }
 }
 
-pub fn chat_window(mut local: ResMut<ChatResource>, mut egui_context: ResMut<EguiContext>, keyboard_input: Res<Input<KeyCode>>, mut chat_writer: EventWriter<ChatEvent>) {
+pub fn chat_window(mut local: ResMut<ChatResource>, mut egui_context: ResMut<EguiContext>, mut chat_writer: EventWriter<ChatEvent>) {
     egui::Window::new("Chat Window").show(egui_context.ctx_mut(), |ui| {
         egui::ScrollArea::new([false, true]).auto_shrink([false, false]).max_height(500.0).stick_to_bottom().show(ui, |ui|{
-            ui.label(local.chat_history.to_string());
+            for mut line in local.chat_history.to_string_vec() {
+                ui.add(egui::TextEdit::multiline(&mut line).margin(egui::Vec2{x: 4.0, y: 0.0}).desired_rows(1).desired_width(ui.available_width()).frame(false));
+            }
         });
 
         let chat_input_response = ui.add(egui::TextEdit::singleline(&mut local.chat_input_text).desired_width(ui.available_width()));
-        chat_input_response.request_focus();
-
-        if chat_input_response.has_focus() && keyboard_input.just_pressed(KeyCode::Return) {
+        
+        if chat_input_response.lost_focus() && ui.input().key_pressed(egui::Key::Enter) {
             if !local.chat_input_text.starts_with("/") {
                 local.chat_input_text = format!("/pub {}", local.chat_input_text.to_string())
             }
@@ -73,6 +80,7 @@ pub fn chat_window(mut local: ResMut<ChatResource>, mut egui_context: ResMut<Egu
                 message: local.chat_input_text.clone()
             });
             local.chat_input_text = String::new();
+            chat_input_response.request_focus();
         }
     });
     egui::Window::new("Help Window").show(egui_context.ctx_mut(), |ui| {
@@ -96,7 +104,7 @@ pub fn chat_window(mut local: ResMut<ChatResource>, mut egui_context: ResMut<Egu
 }
 trait ChatHistory{
     fn add_line(&mut self, line: ChatEvent);
-    fn to_string(&self) -> String;
+    fn to_string_vec(&self) -> Vec<String>;
 }
 
 impl ChatHistory for VecDeque<ChatEvent> {
@@ -107,15 +115,8 @@ impl ChatHistory for VecDeque<ChatEvent> {
         }
     }
     
-    fn to_string(&self) -> String {
-        let mut history_text = String::new();
-        if self.len() > 0 {
-            history_text = self[0].to_string();
-            for i in 1..self.len() {
-                history_text = format!("{}\n{}", history_text, self[i].to_string());
-            }
-        }
-        return history_text;
+    fn to_string_vec(&self) -> Vec<String> {
+        return self.into_iter().map(|a| a.to_string()).collect();
     }
 }
 
